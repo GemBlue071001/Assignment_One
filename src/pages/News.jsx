ce0eb584a6b187d5a11ae5826a118fb4be7a968a
trinhtam <trinhtam2001@gmail.com>
2025-10-22 21:23:02 +0700

    news components

--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,8 +1,34 @@
+import { newLists } from "../constants/news"
 const News = () => {
+
+    const news = newLists
+
     return (
         <div className="container mt-4">
-            <h1>Latest News</h1>
-            <p>Stay updated with our latest news and announcements.</p>
+            <h1>News Category</h1>
+            <div className="row">
+                {
+                   news.map((newsItem) => {
+                        return (
+                            <div key={newsItem.id} className="col-md-6 col-lg-4 mb-4">
+                                <div className="card h-100">
+                                    <img
+                                        src={newsItem.images}
+                                        className="card-img-top"
+                                        alt={newsItem.title}
+                                        style={{ height: '200px', objectFit: 'cover' }}
+                                    />
+                                    <div className="card-body d-flex flex-column">
+                                        <h5 className="card-title">{newsItem.title}</h5>
+                                        <p className="card-text flex-grow-1">{newsItem.description}</p>
+                                        <button className="btn btn-primary mt-auto">Read More</button>
+                                    </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
